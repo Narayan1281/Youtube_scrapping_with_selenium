27ec8cb4ee9435ceb14e15dfcc9ad615e886b25e
rustam <b20128@students.iitmandi.ac.in>
2023-06-29 18:34:28 +0530

    #3: selenium starter

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Youtube_scrapping_with_selenium
 This project is intended to scrap top 10 youtube trending videos. (For learning purpose)
+
+### Useful Resources:
+- [Selenium installation and setup guide](https://medium.com/featurepreneur/selenium-and-web-driver-setup-for-ubuntu-4c41cb61cb6f)
---
+++ scraper_adv.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+YOUTUBE_TRENDING_URL = "https://www.youtube.com/feed/trending"
+
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  
+    # chrome_options.add_argument("--disable-infobars")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+if __name__ == "__main__":
+    print('Creating driver')
+    driver = get_driver()
+    
+    print('Fetching the page...')
+    driver.get(YOUTUBE_TRENDING_URL)
+
+    print('Page title: ', driver.title)
+
+    
